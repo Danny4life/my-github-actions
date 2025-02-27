dadcb1d2375feb3968e41055996992eb4bfab19f
Danny4life <osiki.segun@gmail.com>
2025-02-27 08:57:58 +0100

    edit docker configuration

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM openjdk:8
+FROM openjdk:17
 EXPOSE 8080
 ADD target/demo-image.jar demo-image.jar
 ENTRYPOINT ["java","-jar","/demo-image.jar"]
--- src/main/java/com/osiki/github_actions/GithubActionsApplication.java
+++ src/main/java/com/osiki/github_actions/GithubActionsApplication.java
@@ -28,6 +28,18 @@ public class GithubActionsApplication {
  * 4. Go to Actions and select java with maven
  * 5. edit the name and run
  * 6. commit the changes to a new branch
+ * 7. write the docker command on maven.yml file
+ * 8. signin to docker hub and create a repo
+ * 9. add the repo name to the image
+ * 10. add the jar name demo image on pom.xml inside finalName tag
+ * 11. create a dockerfile inside the root
+ * 12. create secrets DOCKER_USERNAME (aniel1089) and DOCKER PASSWORD(SCHool0000dummy) on github repo - settings - secret and variables - new actions - repository
+ * 13. push to github
+ * 14. go to docker hub to check the image pushed
+ * 15. start docker desktop so you can pull the image
+ * 16. open the terminal and paste the docker pull command gotten from docker hub
+ * 17. run docker images - to check list of images
+ * 18. run the docker image - docker run -p 8080:8080 aniel1089/demo-image:latest
  */
 
 }
